4b8345364784b5f7b96588ee666d967134fac15f
JuhoLeppakangas <ae9937@student.jamk.fi>
2024-09-05 14:36:18 +0300

    komponentit toimii

--- tc-dungeonhelper/src/app/highway/highway.component.ts
+++ tc-dungeonhelper/src/app/highway/highway.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-highway',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, RouterOutlet],
   templateUrl: './highway.component.html',
   styleUrl: './highway.component.css'
 })
