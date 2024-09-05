a48ecf91081ffa65851882c5cbb1364a72b24550
peltimies <ac9194@student.jamk.fi>
2024-09-05 13:48:01 +0300

    dashboard

--- tc-dungeonhelper/src/app/app.routes.ts
+++ tc-dungeonhelper/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { RandomComponent } from './random/random.component';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 export const routes: Routes = [
-  { path: '', redirectTo: '/app', pathMatch: 'full' },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'random', component: RandomComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
