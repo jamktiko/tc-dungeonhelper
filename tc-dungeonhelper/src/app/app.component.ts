0b86fa58346364bd0cc592bd3f175e554d0bf300
peltimies <ac9194@student.jamk.fi>
2024-09-05 13:57:18 +0300

    Navin jalostus

--- tc-dungeonhelper/src/app/app.component.ts
+++ tc-dungeonhelper/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, DashboardComponent],
+  imports: [RouterOutlet, RouterModule, NavbarComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- tc-dungeonhelper/src/app/app.routes.ts
+++ tc-dungeonhelper/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { RandomComponent } from './random/random.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/app', pathMatch: 'full' },
   { path: 'random', component: RandomComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
