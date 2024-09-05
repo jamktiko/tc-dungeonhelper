5b812359ced5183795d3f7f4399ecea6ca521404
JuhoLeppakangas <ae9937@student.jamk.fi>
2024-09-05 14:08:30 +0300

    komponentteja lisää

---
+++ tc-dungeonhelper/src/app/dungeon/dungeon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dungeon',
+  standalone: true,
+  imports: [],
+  templateUrl: './dungeon.component.html',
+  styleUrl: './dungeon.component.css'
+})
+export class DungeonComponent {
+
+}
---
+++ tc-dungeonhelper/src/app/wilderness/wilderness.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wilderness',
+  standalone: true,
+  imports: [],
+  templateUrl: './wilderness.component.html',
+  styleUrl: './wilderness.component.css'
+})
+export class WildernessComponent {
+
+}
