1a191eba22ecafcc7351c84acb969d7fa6db96a1
peltimies <ac9194@student.jamk.fi>
2024-09-13 12:50:01 +0300

    ok

--- tc-dungeonhelper/src/app/app.routes.ts
+++ tc-dungeonhelper/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { RandomComponent } from './random/random.component';
 import { HighwayComponent } from './highway/highway.component';
-import { DungeonComponent } from './dungeon/dungeon.component';
-import { SettlementComponent } from './settlement/settlement.component';
-import { WildernessComponent } from './wilderness/wilderness.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EditHighwayComponent } from './edit-highway/edit-highway.component';
 
@@ -13,8 +11,6 @@ export const routes: Routes = [
   { path: 'random', component: RandomComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'highway', component: HighwayComponent },
-  { path: 'dungeon', component: DungeonComponent },
-  { path: 'settlement', component: SettlementComponent },
-  { path: 'wilderness', component: WildernessComponent },
-  { path: 'edit-highway', component: EditHighwayComponent},
+
+  { path: 'edit-highway', component: EditHighwayComponent },
 ];
