9c71196a891b34fd73a0579539f0ca90415b2b50
peltimies <ac9194@student.jamk.fi>
2024-09-17 09:59:14 +0300

    siirto in-memory dataan

--- tc-dungeonhelper/src/app/eservice.service.ts
+++ tc-dungeonhelper/src/app/eservice.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Enc } from './enc';
 import { catchError, Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EserviceService {
-  private apiUrl = 'api/highwayEncs';
+  private apiUrl = 'api/randomEncs'; // API URL to get the random encounters
   constructor(private http: HttpClient) {}
 
   private handleError(error: any): Observable<Enc[]> {
@@ -16,6 +17,17 @@ export class EserviceService {
   }
 
   getEncounters(): Observable<Enc[]> {
-    return this.http.get<Enc[]>(this.apiUrl).pipe(catchError(this.handleError));
+    return this.http.get<any>(this.apiUrl).pipe(
+      map((data) => {
+        // Here we extract the 'hEnc' array from within the nested structure
+        return data[0].REL[0].hEnc;
+      }),
+      catchError(this.handleError)
+    );
   }
 }
+
+//  getEncounters(): Observable<Enc[]> {
+//    return this.http.get<Enc[]>(this.apiUrl).pipe(catchError(this.handleError));
+//  }
+//
--- tc-dungeonhelper/src/app/in-memory.service.ts
+++ tc-dungeonhelper/src/app/in-memory.service.ts
@@ -7,58 +7,106 @@ import { Enc } from './enc';
 })
 export class InMemoryService implements InMemoryDbService {
   createDb() {
-    const highwayEncs = [
+    const randomEncs = [
       {
-        id: 1,
-        name: 'Highwaymen',
-        description:
-          ' A group of bandits, armed to the teeth, are lying in wait to ambush unsuspecting travelers.',
-      },
-      {
-        id: 2,
-        name: 'Niko',
-        description:
-          'Niko teleports behind the party and attempts to whip one of the players with an extension cord.',
-      },
-      {
-        id: 3,
-        name: 'Travelling Merchant',
-        description:
-          'A lone merchant, heavily laden with goods, is making their way down the highway. They may offer to trade or sell goods to the party.',
-      },
-      {
-        id: 4,
-        name: 'Group of Pilgrims',
-        description:
-          'A group of devout pilgrims, traveling to a nearby holy site, are seeking donations or food from the party.',
-      },
-      {
-        id: 5,
-        name: 'Wanted Poster',
-        description:
-          'A posted notice on a nearby tree or milestone announces a reward for the capture of a notorious outlaw, who may be hiding in the nearby area. ',
-      },
-      {
-        id: 6,
-        name: 'Abandoned Cart',
-        description:
-          'A rickety cart, abandoned by its owner, contains a few scattered goods and a cryptic note hinting at a local mystery',
-      },
-      {
-        id: 7,
-        name: 'Mysterious Stranger',
-        description:
-          'A hooded figure, dressed in dark clothing, is watching the party from a distance.',
-      },
-      {
-        id: 8,
-        name: 'Roadside Shrine',
-        description:
-          'A small, old shrine dedicated to a deity stands on the side of the road.',
+        REL: [
+          {
+            highway: 'highway',
+            hEnc: [
+              {
+                id: 1,
+                name: 'Highwaymen',
+                description:
+                  'A group of bandits, armed to the teeth, are lying in wait to harass unsuspecting travelers.',
+              },
+              {
+                id: 2,
+                name: 'Niko',
+                description:
+                  'Niko teleports behind the party and attempts to whip one of the players with an extension cord.',
+              },
+              {
+                id: 3,
+                name: 'Travelling Merchant',
+                description:
+                  'A lone merchant, heavily laden with goods, is making their way down the highway. They may offer to trade or sell goods to the party.',
+              },
+              {
+                id: 4,
+                name: 'Group of Pilgrims',
+                description:
+                  'A group of devout pilgrims, traveling to a nearby holy site, are seeking donations or food from the party.',
+              },
+              {
+                id: 5,
+                name: 'Wanted Poster',
+                description:
+                  'A posted notice on a nearby tree or milestone announces a reward for the capture of a notorious outlaw, who may be hiding in the nearby area. ',
+              },
+              {
+                id: 6,
+                name: 'Abandoned Cart',
+                description:
+                  'A rickety cart, abandoned by its owner, contains a few scattered goods and a cryptic note hinting at a local mystery',
+              },
+              {
+                id: 7,
+                name: 'Mysterious Stranger',
+                description:
+                  'A hooded figure, dressed in dark clothing, is watching the party from a distance.',
+              },
+              {
+                id: 8,
+                name: 'Roadside Shrine',
+                description:
+                  'A small, old shrine dedicated to a deity stands on the side of the road.',
+              },
+            ],
+            dungeon: 'dungeon',
+            dEnc: [
+              {
+                id: 9,
+                name: 'Bandits',
+                description:
+                  'A group of bandits, armed to the teeth, are lying in wait to harass unsuspecting travelers.',
+              },
+              {
+                id: 10,
+                name: 'Merchant',
+                description:
+                  'A lone merchant, heavily laden with goods, is making their way down the highway. They may offer to trade or sell goods to the party.',
+              },
+              {
+                id: 11,
+                name: 'Pilgrims',
+                description:
+                  'A group of devout pilgrims, traveling to a nearby holy site, are seeking donations or food from the party.',
+              },
+            ],
+            settlement: 'settlement',
+            sEnc: [
+              {
+                id: 12,
+                name: 'Abandoned House',
+                description:
+                  'A rickety cart, abandoned by its owner, contains a few scattered goods and a cryptic note hinting at a local mystery',
+              },
+            ],
+            wilderness: 'wilderness',
+            wEnc: [
+              {
+                id: 13,
+                name: 'Abandoned Mine',
+                description:
+                  'A rickety cart, abandoned by its owner, contains a few scattered goods and a cryptic note hinting at a local mystery',
+              },
+            ],
+          },
+        ],
       },
     ];
 
-    return { highwayEncs };
+    return { randomEncs };
   }
 
   genId(encounters: Enc[]): number {
