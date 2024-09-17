7e3bedaabf02fa7442172db3d9beaaec112cdf10
peltimies <ac9194@student.jamk.fi>
2024-09-17 11:52:31 +0300

    ei toimi

--- tc-dungeonhelper/src/app/highway/highway.component.ts
+++ tc-dungeonhelper/src/app/highway/highway.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule, NgFor } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { Enc } from '../enc';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule, RouterOutlet } from '@angular/router';
 import { EserviceService } from '../eservice.service';
-import { DicerollService } from '../diceroll.service';
+
+import { REL } from '../enc';
 
 @Component({
   selector: 'app-highway',
@@ -12,20 +12,32 @@ import { DicerollService } from '../diceroll.service';
   templateUrl: './highway.component.html',
   styleUrls: ['./highway.component.css'],
 })
-export class HighwayComponent {
-  randomEncs: Enc[] = [];
-  rolledEncounter: Enc | null = null; // Store the whole rolled encounter object
+export class HighwayComponent implements OnInit {
+  rE: REL | undefined;
 
-  constructor(private eservice: EserviceService, private drs: DicerollService) {
-    this.eservice.getEncounters().subscribe(
-      (data) => (this.randomEncs = data),
-      (error) => console.error(error)
-    );
-  }
+ // rolledEncounter: any | null = null;
+ // currentEncounterType: string = 'highway'; // Default encounter type
 
-  rollTable() {
-    const randomEncounter = this.drs.rollForEntity(this.randomEncs);
-    this.rolledEncounter =
-      this.randomEncs.find((enc) => enc.name === randomEncounter) || null; // Store the whole object
-  }
-}
+  constructor(private eservice: EserviceService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const pageId = Number(params.get('pageId'));
+      this.eservice.getEncounters().subscribe((data) => {
+        this.rE = data.find((data) => data.id === pageId);
+      })
+    })
+
+  // Function to roll a random encounter from the current encounter type
+
+///    const randomEncounter = this.drs.rollForEntity(encountersList);
+///    this.rolledEncounter =
+///      encountersList.find((enc) => enc.name === randomEncounter) || null;
+///  }
+///
+///  // Method to switch the current encounter type
+///  setEncounterType(type: string) {
+///    this.currentEncounterType = type;
+///    this.rolledEncounter = null; // Reset rolled encounter when changing type
+///  }
+///}
