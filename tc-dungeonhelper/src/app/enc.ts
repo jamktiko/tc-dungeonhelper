219c691efa6520462d1ca600c12ee7ae5217ada8
peltimies <ac9194@student.jamk.fi>
2024-09-17 11:38:58 +0300

    Kokeilaas ny saaha tää

--- tc-dungeonhelper/src/app/enc.ts
+++ tc-dungeonhelper/src/app/enc.ts
@@ -1,7 +1,15 @@
+//Interface tulevalle datalle, "Encounter" toimintoon.
 export interface Enc {
   id: number;
   name: string;
   description: string;
 }
 
-//Interface tulevalle datalle, "Encounter" toimintoon.
+export interface REL {
+  biome?: string;
+  enc?: Enc[];
+}
+
+export interface InMemory {
+  REL: REL[];
+}
