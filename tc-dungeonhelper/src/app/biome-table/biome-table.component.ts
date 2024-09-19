9ccf043ac279fb55c2a19de3471fb00decc78a54
peltimies <ac9194@student.jamk.fi>
2024-09-19 11:04:33 +0300

    Initial commit

---
+++ tc-dungeonhelper/src/app/biome-table/biome-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-biome-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './biome-table.component.html',
+  styleUrl: './biome-table.component.css'
+})
+export class BiomeTableComponent {
+
+}
