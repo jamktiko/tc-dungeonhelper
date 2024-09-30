821bb794c8ff829c414e32c06aac925aef001021
nigelangelo <ad0986@student.jamk.fi>
2024-09-30 10:51:03 +0300

    pasdadsafasfadf

--- tc-dungeonhelper/src/app/in-memory.service.ts
+++ tc-dungeonhelper/src/app/in-memory.service.ts
@@ -17,48 +17,56 @@ export class InMemoryService implements InMemoryDbService {
             name: 'Highwaymen',
             description:
               'A group of bandits, armed to the teeth, are lying in wait to harass unsuspecting travelers.',
+            weight: 1,
           },
           {
             id: 2,
             name: 'Niko',
             description:
               'Niko teleports behind the party and attempts to whip one of the players with an extension cord.',
+            weight: 1,
           },
           {
             id: 3,
             name: 'Travelling Merchant',
             description:
               'A lone merchant, heavily laden with goods, is making their way down the highway. They may offer to trade or sell goods to the party.',
+            weight: 1,
           },
           {
             id: 4,
             name: 'Group of Pilgrims',
             description:
               'A group of devout pilgrims, traveling to a nearby holy site, are seeking donations or food from the party.',
+            weight: 1,
           },
           {
             id: 5,
             name: 'Wanted Poster',
             description:
               'A posted notice on a nearby tree or milestone announces a reward for the capture of a notorious outlaw, who may be hiding in the nearby area. ',
+            weight: 1,
           },
           {
             id: 6,
             name: 'Abandoned Cart',
             description:
               'A rickety cart, abandoned by its owner, contains a few scattered goods and a cryptic note hinting at a local mystery',
+            weight: 1,
           },
           {
             id: 7,
             name: 'Mysterious Stranger',
             description:
               'A hooded figure, dressed in dark clothing, is watching the party from a distance.',
+            weight: 1,
           },
           {
             id: 8,
             name: 'Roadside Shrine',
             description:
               'A small, old shrine dedicated to a deity stands on the side of the road.',
+            weight: 1,
           },
         ],
       },
@@ -70,18 +78,22 @@ export class InMemoryService implements InMemoryDbService {
             name: 'Spider`s web',
             description:
               'A narrow corridor is filled with a thick layer of spider webs. ',
+            weight: 1,
           },
           {
             id: 10,
             name: 'Rival Dungeon Crawlers',
             description:
               'A rival gang of 2d4 Dungeon Crawlers are heard fighting a group of 2d6 Goblins in an adjacent room',
+            weight: 1,
           },
           {
             id: 11,
             name: 'Skeletons',
             description:
               '2d6 Skeletal sentinels are guarding an artifact placed on a pedestal. Their bones creak with a malevolent rattle. ',
+
+            weight: 1,
           },
         ],
       },
@@ -93,6 +105,7 @@ export class InMemoryService implements InMemoryDbService {
             name: 'Paranoid Guards',
             description:
               'A patrol of stalwart defenders of the realm are convinced that you are hiding something. Probably several somethings. In fact, they accuse the party of being a spy, a thief, a murdered and a pastry thief all rolled into one.',
+            weight: 1,
           },
         ],
       },
@@ -103,6 +116,7 @@ export class InMemoryService implements InMemoryDbService {
             id: 13,
             name: 'Abandoned Mine',
             description: 'A raiding team of 2d4 Orcs run through the forest.',
+            weight: 1,
           },
         ],
       },
