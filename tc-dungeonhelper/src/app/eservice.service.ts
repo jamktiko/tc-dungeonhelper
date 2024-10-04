77eb4d4eaaf261e04f907f25c8501483bdb6c566
Peltimies <ac9194@student.jamk.fi>
2024-10-04 18:50:03 +0300

    Yritys siirtyä backäriin

--- tc-dungeonhelper/src/app/eservice.service.ts
+++ tc-dungeonhelper/src/app/eservice.service.ts
@@ -1,29 +1,39 @@
 import { Injectable } from '@angular/core';
 import { RandomEncounters } from './types';
 import { catchError, map, Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const headers = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class EserviceService {
-  private apiUrl = 'api/randomEncounters';
-  private tableUrl = 'api/encounters';
+  private apiUrl = 'http://localhost:3000/randomEncounters';
 
   constructor(private http: HttpClient) {}
 
-  private handleError<T>(error: any): Observable<T[]> {
+  // private handleError<T>(error: any): Observable<T[]> {
+  //   console.error('An error occurred', error);
+  //   return of([] as T[]); // Return an empty array on error
+  // }
+
+  private handleError(error: any): Observable<any> {
     console.error('An error occurred', error);
-    return of([] as T[]); // Return an empty array on error
+    return error.message || error;
   }
-  getEncounters(): Observable<RandomEncounters[]> {
-    return this.http.get<RandomEncounters[]>(this.apiUrl);
+  getEncounters(): Observable<any> {
+    return this.http.get<any>(this.apiUrl).pipe(catchError(this.handleError));
   }
 
-  getOneEncouter(): Observable<RandomEncounters[]> {
+  getTable(): Observable<RandomEncounters[]> {
     return this.http
       .get<RandomEncounters[]>(this.apiUrl)
-      .pipe(catchError((error) => this.handleError<RandomEncounters>(error)));
+      .pipe(catchError(this.handleError));
   }
 
   /**
@@ -31,12 +41,12 @@ export class EserviceService {
    * @param id The id of the encounter table to be retrieved.
    * @returns An observable of the requested encounter table.
    */
-  getTable(): Observable<RandomEncounters[]> {
-    return this.http.get<RandomEncounters[]>(this.apiUrl).pipe(
-      catchError((error) => this.handleError<RandomEncounters>(error)),
-      map((encounters) => {
-        return encounters.filter((encounter) => encounter.enc);
-      })
-    );
-  }
+  //  getTable(): Observable<RandomEncounters[]> {
+  //    return this.http.get<RandomEncounters[]>(this.apiUrl).pipe(
+  //      catchError((error) => this.handleError<RandomEncounters>(error)),
+  //      map((encounters) => {
+  //        return encounters.filter((encounter) => encounter.enc);
+  //      })
+  //    );
+  //  }
 }
