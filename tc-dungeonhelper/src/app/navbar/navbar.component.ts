c8589d8b2a9c6ade831df1ed08094519eb395937
JuhoLeppakangas <ae9937@student.jamk.fi>
2024-10-21 20:11:57 +0300

    navbar reaktiivinen

--- tc-dungeonhelper/src/app/navbar/navbar.component.ts
+++ tc-dungeonhelper/src/app/navbar/navbar.component.ts
@@ -4,19 +4,24 @@ import { RetablesComponent } from '../retables/retables.component';
 import { MerchantsComponent } from '../merchants/merchants.component';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RetablesComponent, RouterModule,MerchantsComponent, MatButtonModule, CommonModule],
+  imports: [RouterOutlet, RetablesComponent, RouterModule,MerchantsComponent, MatButtonModule, CommonModule, MatToolbarModule, MatSidenav, MatSidenavModule, MatIconModule, MatListModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  menuOpen = false;
+  toggleSidenav(sidenav: any) {
+    sidenav.toggle(); // Kutsutaan sidenavin toggle-funktiota
+  }
 
-
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
+  closeSidenav(sidenav: any) {
+    sidenav.close(); // Suljetaan sidenav
   }
 }
