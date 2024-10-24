b94e90c3fecec575e78d6e71bf3d22052a7ea6ff
peltimies <ac9194@student.jamk.fi>
2024-10-24 14:42:16 +0300

    ASD

--- tc-dungeonhelper/src/app/app.component.ts
+++ tc-dungeonhelper/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, NavbarComponent],
+  imports: [RouterOutlet, RouterModule, NavbarComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
