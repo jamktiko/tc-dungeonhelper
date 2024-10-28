d9b5587972e9d72e67014ff4a05154a459a816fe
peltimies <ac9194@student.jamk.fi>
2024-10-28 10:19:29 +0200

    ASD

--- tc-dungeonhelper/src/app/app.routes.ts
+++ tc-dungeonhelper/src/app/app.routes.ts
@@ -4,13 +4,15 @@ import { EnctableComponent } from './enctable/enctable.component';
 import { RetablesComponent } from './retables/retables.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MerchantsComponent } from './merchants/merchants.component';
+import { InventoryComponent } from './inventory/inventory.component';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   { path: 'retables', component: RetablesComponent },
 
   { path: 'biome/:biome', component: EnctableComponent },
+  { path: 'merchant/:merchant', component: InventoryComponent },
 
   { path: 'merchants', component: MerchantsComponent },
-  { path: 'dashboard', component: DashboardComponent}
+  { path: 'dashboard', component: DashboardComponent },
 ];
