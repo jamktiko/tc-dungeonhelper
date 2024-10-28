8957e14f39e88e2b7018bd0df10532a9768ce1b2
JuhoLeppakangas <ae9937@student.jamk.fi>
2024-10-28 11:40:45 +0200

    footer

--- tc-dungeonhelper/src/app/footer/footer.component.ts
+++ tc-dungeonhelper/src/app/footer/footer.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
 }
