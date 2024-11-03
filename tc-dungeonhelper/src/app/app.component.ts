c67897adc8db550e0afa3d45c502b151aaecb007
JuhoLeppakangas <ae9937@student.jamk.fi>
2024-11-03 12:55:19 +0200

    karuselli

--- tc-dungeonhelper/src/app/app.component.ts
+++ tc-dungeonhelper/src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';  // Tuodaan CommonModule
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FooterComponent } from './footer/footer.component';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { KaruselliComponent } from "./karuselli/karuselli.component";
+
+interface CarouselItem {
+  image: string;
+  title: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -10,13 +19,18 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     RouterOutlet,
     RouterModule,
+    CommonModule, // Lisätään CommonModule imports-taulukkoon
     NavbarComponent,
     DashboardComponent,
     FooterComponent,
-  ],
+    MatButtonModule,
+    MatButton,
+    KaruselliComponent
+],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Dunkkuapuri';
+
 }
