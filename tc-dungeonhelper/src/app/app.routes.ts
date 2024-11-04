bcb6b53724129bdc43c942de5a1b5a8377ac370f
peltimies <ac9194@student.jamk.fi>
2024-11-04 13:28:56 +0200

    Turha komponentti poistettu, nyt hiotaan randomencounterin statsit ym.

--- tc-dungeonhelper/src/app/app.routes.ts
+++ tc-dungeonhelper/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { EnctableComponent } from './enctable/enctable.component';
 import { RetablesComponent } from './retables/retables.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MerchantsComponent } from './merchants/merchants.component';
-import { InventoryComponent } from './inventory/inventory.component';
+
 import { AppComponent } from './app.component';
 import { MerchantDetailComponent } from './merchant-detail/merchant-detail.component';
 import { KaruselliComponent } from './karuselli/karuselli.component';
@@ -13,9 +13,9 @@ export const routes: Routes = [
   { path: 'retables', component: RetablesComponent },
 
   { path: 'biome/:biome', component: EnctableComponent },
-  { path: 'merchant/', component: InventoryComponent },
+
   { path: 'merchants/:id', component: MerchantDetailComponent },
-  { path: '', component: KaruselliComponent},
+  { path: '', component: KaruselliComponent },
   { path: 'merchants', component: MerchantsComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
