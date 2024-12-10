31299ece3990db1a015b84a5da5fa532736b52d5
JuhoLeppakangas <ae9937@student.jamk.fi>
2024-12-10 11:07:05 +0200

    df

--- tc-dungeonhelper/src/environments/environment.ts
+++ tc-dungeonhelper/src/environments/environment.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: true,
   url: 'http://dunkku.eu-north-1.elasticbeanstalk.com/randomEncounters',
-<<<<<<< HEAD
-  merchantUrl: 'http://localhost:3000/merchants',
-=======
   merchantUrl: 'http://dunkku.eu-north-1.elasticbeanstalk.com/merchants',
->>>>>>> db119bc301f7e544cb34551bec97a53930843f22
 };
