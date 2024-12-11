e72d30795476907ea1372cee3377444397581827
peltimies <ac9194@student.jamk.fi>
2024-12-11 10:02:43 +0200

    fix: Korjattu CORS-ongelma lisäämällä /api-etuliite kauppiaiden reittiin
    - Muutettu kauppiaiden API-polku muotoon /api/merchants
    - Päivitetty ympäristömuuttujat vastaamaan uutta API-polkua
    - Erotettu frontend- ja backend-reitit toisistaan selkeämmällä rakenteella
    
    Tämä korjaus estää sekaannuksen frontend-reitityksen ja API-endpointtien välillä.

--- tc-dungeonhelper/src/environments/environment.development.ts
+++ tc-dungeonhelper/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   url: 'http://localhost:3000/randomEncounters',
-  merchantUrl: 'http://localhost:3000/merchants',
+  merchantUrl: 'http://localhost:3000/api/merchants',
   userUrl: 'http://localhost:3000/users'
 };
--- tc-dungeonhelper/src/environments/environment.ts
+++ tc-dungeonhelper/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'https://dunkku.eu-north-1.elasticbeanstalk.com/randomEncounters',
-  merchantUrl: 'https://dunkku.eu-north-1.elasticbeanstalk.com/merchants',
+  merchantUrl: 'https://dunkku.eu-north-1.elasticbeanstalk.com/api/merchants',
   userUrl: 'https://dunkku.eu-north-1.elasticbeanstalk.com/users'
 };
